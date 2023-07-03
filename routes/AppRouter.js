8d0b861530a1d3d673c6d6425ddcae0aa2a3e791
Maxsos133 <max.pavlenko133@gmail.com>
2023-07-03 15:02:55 -0500

    fix vercel

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,8 +2,10 @@ const express = require('express')
 const Router = express.Router()
 const PlanetRouter = require(`./planetsRouter.js`)
 const MoonRouter = require(`./moonsRouter.js`)
+const BodyRouter = require(`./bodiesRouter.js`)
 
 Router.use(`/planets`, PlanetRouter)
-Router.use( `/moons` , MoonRouter)
+Router.use(`/moons` , MoonRouter)
+Routeer.use(`/boides`,BodyRouter )
 
 module.exports = Router
