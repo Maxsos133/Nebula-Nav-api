66c4da73cc88f8d66c3a39e193a911ec122343d0
Christopher Erb <christophererb01@gmail.com>
2023-07-03 20:30:33 -0400

    seed number format standardization

--- seeds/bodySeed.js
+++ seeds/bodySeed.js
@@ -6,16 +6,16 @@ const main = async () => {
     const bodies = [
         {
             name: `Sun`,
-            mass: `1.989 x 10^30 kg`,
+            mass: `1.989 x 10³⁰ kg`,
             diameter: `1392700 km`,
-            dist_from_sun: `0`,
+            dist_from_sun: `0 km`,
             terrain: `gas`,
             surface: `burning plasma hydrogen`,
             surface_temperature: `5600°C`,
             has_water: `no`,
             has_aliens: `possibly`,
             gravity: `274 m/s²`,
-            velocity: `720000 km/h`,
+            velocity: `720,000 km/h`,
             mapData: {
               coordinates: {
                 longitude: `0`,
